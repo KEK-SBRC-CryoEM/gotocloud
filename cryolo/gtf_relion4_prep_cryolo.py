df4ac0d994fb1bdfee9af885ee60c4b3fdf0283b
kek-yamamoto00 <misatoy@post.kek.jp>
2024-09-25 12:40:34 +0900

    Add copyright

--- cryolo/gtf_relion4_prep_cryolo.py
+++ cryolo/gtf_relion4_prep_cryolo.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python2.7
 
-# Author: Toshio Moriya 2021-Current (toshio.moriya@kek.jp)
+# ***************************************************************************
 #
-# Copyright (c) 2021 KEK IMMS SBRC
+# Copyright (c) 2021-2024 Structural Biology Research Center, 
+#                         Institute of Materials Structure Science, 
+#                         High Energy Accelerator Research Organization (KEK)
+#
+#
+# Authors:   Toshio Moriya (toshio.moriya@kek.jp)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+#
+# ***************************************************************************
+#
 #
 # [Develop Notes]
 # - 2021/10/10 Toshio Moriya: Add training support
