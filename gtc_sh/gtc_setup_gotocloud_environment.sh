270e8ce78a5cd301fe03caadc19499f926a949e8
kek-yamamoto00 <misatoy@post.kek.jp>
2024-09-25 10:25:46 +0900

    Release v00.06.09 (add copyright)

--- gtc_sh/gtc_setup_gotocloud_environment.sh
+++ gtc_sh/gtc_setup_gotocloud_environment.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 #
+# ***************************************************************************
+#
+# Copyright (c) 2021-2024 Structural Biology Research Center, 
+#                         Institute of Materials Structure Science, 
+#                         High Energy Accelerator Research Organization (KEK)
+#
+#
+# Authors:   Toshio Moriya (toshio.moriya@kek.jp)
+#            Misato Yamamoto (misatoy@post.kek.jp)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+#
+# ***************************************************************************
+#
 # Usage:
 #  gtc_setup_gotocloud_environment.sh
 #
