99d0d31b8e464e4d4d9765c6678b3aadd9b4846e
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2025-05-13 09:48:28 +0900

    Update bfactor_plot_v2.py

--- rln_external/bfactor_plot/bfactor_plot_v2.py
+++ rln_external/bfactor_plot/bfactor_plot_v2.py
@@ -869,9 +869,9 @@ def bfactor_main(args, unknown):
             print(" BFACTOR | MESSAGE: Plot written to " + opts.outfilepath_list["rosenthal"])
 
             # additional plot (breakpoint)
-            calc_and_plot_breakpoint(xs=np.array(bfactor_data["log_n_particles"]), 
-                                     ys=np.array(bfactor_data["inv_resolution_squared"]),
-                                     savepath=opts.outfilepath_list["analysis_breakpoint"])
+            # calc_and_plot_breakpoint(xs=np.array(bfactor_data["log_n_particles"]), 
+            #                          ys=np.array(bfactor_data["inv_resolution_squared"]),
+            #                          savepath=opts.outfilepath_list["analysis_breakpoint"])
             
         else:
             print(" BFACTOR | WARNING: Failed to plot. One of these libraries may be missing: matplotlib and/or numpy.\n")
