23693165293f0003f96689092ee03ccd8250a23a
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2026-01-16 14:51:59 +0900

    Update prews_slurm_relion500_cpu.sh
    updated --ntasks-per-node to match default prews settings

--- submission_script/prews_slurm_relion500_cpu.sh
+++ submission_script/prews_slurm_relion500_cpu.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=XXXqueueXXX
 #SBATCH --partition=XXXextra1XXX
 #SBATCH --nodes=XXXextra2XXX
-#SBATCH --ntasks-per-node=XXXdedicatedXXX
+#SBATCH --ntasks-per-node=112
 
 # --- Backup Functions ---
 backup_file () {
